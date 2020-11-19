281a1514bc76bbabf47e80798a9f8a20b136f229
jxy3701 <73856021+jxy3701@users.noreply.github.com>
2020-11-19 19:24:48 -0400

    some mockdata for exercises

---
+++ src/app/mock-exercises.ts
@@ -0,0 +1,14 @@
+import { Exercise } from './exercise';
+
+export const EXERCISES: Exercise[] = [
+  { id: 11, name: 'Ex1' },
+  { id: 12, name: 'Ex2' },
+  { id: 13, name: 'Ex3' },
+  { id: 14, name: 'Ex4' },
+  { id: 15, name: 'Ex5' },
+  { id: 16, name: 'Ex6' },
+  { id: 17, name: 'Ex7' },
+  { id: 18, name: 'Ex8' },
+  { id: 19, name: 'Ex9' },
+  { id: 20, name: 'Ex10' }
+];
