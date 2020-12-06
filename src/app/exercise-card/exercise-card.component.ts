faa90c7c7f74ddabe4ee7fbc8cf154af3cc03eb4
Tejas Patel <tejas.patel@dal.ca>
2020-12-05 22:46:36 -0400

    Create, Edit, Delete functionality for exercise card is updated.

--- src/app/exercise-card/exercise-card.component.ts
+++ src/app/exercise-card/exercise-card.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ExerciseEditDialogComponent } from 'app/exercise-edit-dialog/exercise-edit-dialog.component';
 import { ExerciseCard } from './exercise-card';
+import { ExerciseServiceService } from './exercise-service.service';
 
 @Component({
   selector: 'app-exercise-card',
@@ -10,9 +13,33 @@ export class ExerciseCardComponent implements OnInit {
 
   @Input() exercise: ExerciseCard;
   
-  constructor() { }
+  constructor(private exerciseService: ExerciseServiceService,
+    public dialog: MatDialog) { }
 
   ngOnInit(): void {
+  }
+
+  delete(id: number){
+    this.exerciseService.deleteExercise(id); 
+  }
+
+  editExercise(exercise: ExerciseCard){
+
+    let editExercise = new ExerciseCard();
+    editExercise.id = exercise.id;
+    editExercise.name = ''+exercise.name;
+    editExercise.content = ''+exercise.content;
+
+    const dialogRef = this.dialog.open(ExerciseEditDialogComponent, {
+      width: '300px',
+      data: editExercise
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if(result && result.id){      
+        this.exerciseService.edit(result);
+      }
+    });
   }
 
 }
